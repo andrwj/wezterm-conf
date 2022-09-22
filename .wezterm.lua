43744c2b1b1ca3bbf3e8ce0ad0a8ddf2d2d68533
lkc1713 <lkc1713@gepplus.co.kr>
2022-09-22 13:28:31 +0900

    update@2022-09-22 13:28

---
+++ .wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm';
+return {
+   default_prog = {"/usr/bin/zsh", "-l"},
+   font_size = 10.0,
+   dpi = 96.0,
+   font_shaper = "Harfbuzz",
+   debug_key_events = true,
+   keys = {
+      { key = 'c', mods = 'ALT', action = wezterm.action.Copy },
+      { key = 'v', mods = 'ALT', action = wezterm.action.Paste },
+      { key = '=', mods = 'ALT', action = wezterm.action.IncreaseFontSize },
+      { key = '-', mods = 'ALT', action = wezterm.action.DecreaseFontSize },
+      {
+         key = '\\',
+         mods = 'CTRL|ALT',
+         action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+      },
+      {
+         key = '-',
+         mods = 'CTRL|ALT',
+         action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+      },
+      {
+         key = 't',
+         mods = 'ALT',
+         action = wezterm.action.SpawnTab 'DefaultDomain',
+      },
+      {
+         key = 'n',
+         mods = 'ALT',
+         action = wezterm.action.SpawnWindow,
+      },
+   },
+
+   mouse_bindings = {
+   {
+      event = { Up = { streak = 1, button = 'Middle' } },
+      mods = 'NONE',
+      action = wezterm.action.Paste,
+    },
+  }
+}
